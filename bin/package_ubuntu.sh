ef53c7bbc9a5d18ab0998df8a9d54debd350a465
Joshua Miller <jshuasmiller@gmail.com>
2015-08-10 14:23:06 +0000

    fix(packaging): fix ubuntu packaging
    Closes #5.

--- bin/package_ubuntu.sh
+++ bin/package_ubuntu.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+set -e
+
 # Make sure the correct version of pyinstall is installed
-pip install 'PyInstaller==2.1'
+pip install https://github.com/pyinstaller/pyinstaller/archive/develop.zip
+
 
 # Get version
 VERSION=$(python -c """
@@ -12,8 +15,10 @@ print gdc_client.version.__version__
 # Create binary
 pyinstaller --clean --noconfirm --onefile -c gdc-client
 
-APPNAME="gdc-client"
-SOURCE="dist/gdc-client"
-
 # Zip dist
-zip "gdc-client_${VERSION}_Ubuntu14.04_x64.zip" "${SOURCE}"
+SOURCE_DIR='dist'
+SOURCE="gdc-client"
+ZIP_DIR="${PWD}"
+cd "${SOURCE_DIR}"
+
+zip "${ZIP_DIR}/gdc-client_${VERSION}_Ubuntu14.04_x64.zip" "${SOURCE}"
