2c74567a8c2ff92059378130f25a7925cd39cbcd
MurphyMarkW <murphy.mark.w+github@gmail.com>
2016-03-08 10:52:03 -0600

    feat(parser): adds secure-read function for token files

--- gdc_client/auth/parser.py
+++ gdc_client/auth/parser.py
@@ -1,4 +1,36 @@
+import os
+import stat
 import argparse
+
+from contextlib import closing
+
+
+def read_token_file(path):
+    """ Safely open, read and close a token file.
+    """
+    # TODO review best way to check file security on various platforms
+
+    abspath = os.path.abspath(path)
+    try:
+        stats = os.stat(abspath)
+    except OSError as err:
+        raise argparse.ArgumentTypeError(err)
+
+    invalid_permissions = any([
+        stats.st_mode & stat.S_IRWXG, # Group can R/W/X.
+        stats.st_mode & stat.S_IRWXO, # Other can R/W/X.
+    ])
+
+    if invalid_permissions:
+        raise argparse.ArgumentTypeError('token file is not secured')
+
+    try:
+        ifs = open(abspath, 'r')
+    except IOError as err:
+        raise argparse.ArgumentTypeError(err)
+
+    with closing(ifs):
+        return ifs.read().strip()
 
 
 def config(parser):
@@ -8,10 +40,12 @@ def config(parser):
 
     token_group.add_argument('-t', '--token',
         # TODO add type check for token format
+        default=os.environ.get('GDC_AUTH_TOKEN'),
         help='GDC API auth token string',
     )
 
     token_group.add_argument('--token-file',
-        type=argparse.FileType('rb'),
+        dest='token',
+        type=read_token_file,
         help='GDC API auth token file',
     )
