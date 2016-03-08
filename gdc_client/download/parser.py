33c053fa3ef4309707ca682855da3e66fe06bb5b
MurphyMarkW <murphy.mark.w+github@gmail.com>
2016-03-08 10:52:57 -0600

    feat(parser): hooks up download parser w/ top-level args

--- gdc_client/download/parser.py
+++ gdc_client/download/parser.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 
 from parcel import const
 from parcel import manifest
@@ -9,13 +10,15 @@ from .. import log as logger
 from .client import GDCUDTDownloadClient
 from .client import GDCHTTPDownloadClient
 
+
 def get_client(args, token, **_kwargs):
     kwargs = {
         'token': token,
         'n_procs': args.n_processes,
         'directory': args.dir,
         'segment_md5sums': args.segment_md5sums,
-        'debug': args.debug,
+        # TODO remove debug argument - handled by logger
+        'debug': logging.DEBUG in args.log_levels,
         'http_chunk_size': args.http_chunk_size,
         'save_interval': args.save_interval,
         'download_related_files': args.download_related_files,
@@ -66,8 +69,6 @@ def config(parser):
                         type=manifest.argparse_type,
                         default=list(),
                         help='GDC Download manifest file.')
-#    parser.add_argument('-v', '--verbose', action='store_true',
-#                        help='verbose logging')
     parser.add_argument('-d', '--dir',
                         default=None,
                         help='Directory to download files to. '
@@ -80,9 +81,6 @@ def config(parser):
     parser.add_argument('--no-segment-md5sums', dest='segment_md5sums',
                         action='store_false',
                         help='Calculate inbound segment md5sums and/or verify md5sums on restart')
-#    parser.add_argument('--debug', dest='debug',
-#                        action='store_true',
-#                        help='Print stack traces')
     parser.add_argument('-n', '--n-processes', type=int,
                         default=defaults.processes,
                         help='Number of client connections.')
@@ -98,14 +96,6 @@ def config(parser):
     parser.add_argument('--no-annotations', action='store_false',
                         dest='download_annotations',
                         help='Do not download annotations.')
-
-#    token_args = parser.add_mutually_exclusive_group(required=False)
-#    token_args.add_argument('-t', '--token-file',
-#                            type=lambda x: argparse.FileType('r')(x).read(),
-#                            dest='token',
-#                            help='authentication token file')
-#    token_args.add_argument('-T', '--token', default='', type=str,
-#                            dest='token', help='authentication token')
 
     #############################################################
     #                       UDT options
