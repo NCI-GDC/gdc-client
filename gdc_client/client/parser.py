2ea23ef12eba9e926222c76a017b801691426126
MurphyMarkW <murphy.mark.w+github@gmail.com>
2016-03-08 10:56:41 -0600

    feat(parser): adds environment variables for GDC API host / port

--- gdc_client/client/parser.py
+++ gdc_client/client/parser.py
@@ -1,3 +1,5 @@
+import os
+
 GDC_API_HOST = 'gdc-api.nci.nih.gov'
 GDC_API_PORT = 443
 
@@ -5,11 +7,11 @@ def config(parser):
     """ Configure an argparse parser for use with the GDC Client.
     """
     parser.add_argument('-H', '--host',
-        default=GDC_API_HOST,
+        default=os.environ.get('GDC_API_HOST', GDC_API_HOST),
         help='GDC API host [{host}]'.format(host=GDC_API_HOST),
     )
 
     parser.add_argument('-P', '--port',
-        default=GDC_API_PORT,
+        default=os.environ.get('GDC_API_PORT', GDC_API_PORT),
         help='GDC API port [{port}]'.format(port=GDC_API_PORT),
     )
