e22959f0c01d94b7853b18afe1ffdf6741345d06
MurphyMarkW <murphy.mark.w+github@gmail.com>
2016-04-12 13:14:04 -0500

    feat(upload): adds static schema definition

---
+++ gdc_client/upload/schema.py
@@ -0,0 +1,48 @@
+UPLOAD_MANIFEST_SCHEMA = {
+    "title": "Manifest Schema",
+    "type": "object",
+    "additionalProperties": True,
+    "properties": {
+        "files": {
+            "items": {
+                "type": "object",
+                "properties": {
+                    "id": {
+                        "type": "string",
+                        "pattern": "^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$"
+                    },
+                    "file_name": {
+                        "type": "string"
+                    },
+                    "local_file_path": {
+                        "type": "string"
+                    },
+                    "file_size": {
+                        "type": "integer"
+                    },
+                    "md5sum": {
+                        "type": "string",
+                        "pattern": "^[a-f0-9]{32}$"
+                    },
+                    "type": {
+                        "type": "string"
+                    }
+                },
+                "anyOf": [
+                    {
+                        "required": [
+                            "id",
+                            "file_name"
+                        ]
+                    },
+                    {
+                        "required": [
+                            "id",
+                            "local_file_path"
+                        ]
+                    }
+                ]
+            }
+        }
+    }
+}
