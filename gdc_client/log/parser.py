6465e9787d8c85241ea0447172e111a64536df2b
MurphyMarkW <murphy.mark.w+github@gmail.com>
2016-04-26 15:57:46 -0500

    chore(download): rework logging setup

--- gdc_client/log/parser.py
+++ gdc_client/log/parser.py
@@ -2,18 +2,37 @@ import argparse
 import logging
 import sys
 
+try:
+    import http.client as http_client
+except ImportError:
+    # Python 2
+    import httplib as http_client
+
 from .. import version
 
+
+FORMAT = '%(asctime)s: %(levelname)s: %(message)s'
 
 def setup_logging(args):
     """ Set up logging given parsed logging arguments.
     """
-    logging.root.setLevel(min(args.log_levels))
+    logging.basicConfig(
+        format=FORMAT,
+        level=min(args.log_levels),
+        stream=args.log_file,
+    )
+
+    http_client.HTTPConnection.debuglevel = args.trace
 
 def config(parser):
     """ Configure an argparse parser for logging.
     """
     parser.set_defaults(log_levels=[logging.ERROR])
+
+    parser.add_argument('--trace',
+        action='store_true',
+        help='enable requests tracing',
+    )
 
     parser.add_argument('--debug',
         action='append_const',
