7def4c0a0df994e61eb2feb431bdc50eaeac02dc
Joe Sislow <madopal@uchicago.edu>
2016-10-20 17:16:09 -0500

    feat(py26): Backported changes of latest to branch
    - Backported all the changes for the current version

--- gdc_client/log/log.py
+++ gdc_client/log/log.py
@@ -5,6 +5,42 @@ from parcel import colored
 
 loggers = {}
 
+class LogFormatter(logging.Formatter):
+
+        err_format  = colored('ERROR: ', 'red') + '%(msg)s'
+        warn_format  = colored('WARNING: ', 'yellow') + '%(msg)s'
+        dbg_format  = colored('%(asctime)s - DEBUG: %(module)s: %(lineno)d: ', 'blue') + '%(msg)s'
+        info_format = '%(asctime)s - INFO: %(msg)s'
+
+
+        def __init__(self, fmt='%(asctime)s - %(levelname)s: %(msg)s'):
+            logging.Formatter.__init__(self, fmt)
+
+
+        def format(self, record):
+
+            # Save the original format
+            format_orig = self._fmt
+
+            # Replace the original format with one customized by logging level
+            if record.levelno == logging.DEBUG:
+                self._fmt = LogFormatter.dbg_format
+
+            elif record.levelno == logging.INFO:
+                self._fmt = LogFormatter.info_format
+
+            elif record.levelno == logging.WARNING:
+                self._fmt = LogFormatter.warn_format
+
+            elif record.levelno == logging.ERROR:
+                self._fmt = LogFormatter.err_format
+
+            result = logging.Formatter.format(self, record)
+
+            # Restore the original format
+            self._fmt = format_orig
+
+            return result
 
 # Logging
 def get_logger(name='gdc-client'):
@@ -16,8 +52,7 @@ def get_logger(name='gdc-client'):
     log = logging.getLogger(name)
     log.propagate = False
     if sys.stderr.isatty():
-        formatter = logging.Formatter(
-            colored('%(asctime)s: %(levelname)s: ', 'blue')+'%(message)s')
+        formatter = LogFormatter()
     else:
         formatter = logging.Formatter('%(asctime)s: %(levelname)s: %(message)s')
     handler = logging.StreamHandler(sys.stderr)
