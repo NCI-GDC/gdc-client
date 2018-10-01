96ff29903011fde248fa649caa4238679c04e6ab
Maksim An <anmaxvl@gmail.com>
2018-10-01 11:11:17 -0500

    pass custom config into settings resolver as well

--- gdc_client/settings/parser.py
+++ gdc_client/settings/parser.py
@@ -8,8 +8,8 @@ log = logging.getLogger('gdc-settings')
 
 
 class SettingsResolver(object):
-    def __init__(self):
-        self.config = GDCClientConfigShared()
+    def __init__(self, config_file):
+        self.config = GDCClientConfigShared(config_file)
 
     def ls(self, section, args):
         log.info(self.config.to_display_string(section))
@@ -19,24 +19,24 @@ class SettingsResolver(object):
         return self.ls(section, args)
 
 
-def resolve(section, args):
-    resolver = SettingsResolver()
+def resolve(section, config_file, args):
+    resolver = SettingsResolver(config_file)
     command = args.command
     func = getattr(resolver, command)
     return func(section, args)
 
 
-def config(parser):
+def config(parser, config_file=None):
     sub_parsers = parser.add_subparsers(
         title='sub commands', dest='sub'
     )
 
-    upload_resolver = partial(resolve, 'upload')
+    upload_resolver = partial(resolve, 'upload', config_file)
     upload = sub_parsers.add_parser('upload')
     upload.add_argument('command', choices=['list', 'ls'])
     upload.set_defaults(func=upload_resolver)
 
-    download_resolver = partial(resolve, 'download')
+    download_resolver = partial(resolve, 'download', config_file)
     download = sub_parsers.add_parser('download')
     download.set_defaults(func=download_resolver)
     download.add_argument('command', choices=['list', 'ls'])
