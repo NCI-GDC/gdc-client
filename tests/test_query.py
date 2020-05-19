2e0b206f13251aad58f5aa169564c9c23cdd06c0
asurinsaka <qiaozha@gmail.com>
2020-05-19 10:29:48 -0500

    Feat/dev 161 remove duplicated code (#259)
    DEV-161 refactor tests
    
    refactor code in test_query and confest.py
    move the fixture code of mock_server to conftest.py
    add typing for some tests functions

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,117 +1,60 @@
-import time
-from multiprocessing import Process
-from unittest import TestCase
-
 import pytest
-from gdc_client.parcel.const import HTTP_CHUNK_SIZE
+from typing import List, Iterable, Mapping
 
-import mock_server
 from conftest import uuids
+from gdc_client.parcel.const import HTTP_CHUNK_SIZE
 from gdc_client.query.index import GDCIndexClient
 from gdc_client.query.versions import _chunk_list, get_latest_versions
 
 # default values for flask
-server_host = "http://127.0.0.1"
-server_port = "5000"
-
-# same as --server flag for gdc-client
-base_url = server_host + ":" + server_port
+BASE_URL = "http://127.0.0.1:5000"
 
 
-class QueryIndexTest(TestCase):
-    def setUp(self):
-        self.server = Process(target=mock_server.app.run)
-        self.server.start()
+@pytest.mark.usefixtures("setup_mock_server")
+class TestQueryIndex:
+    def setup_method(self):
+        self.index = GDCIndexClient(uri=BASE_URL)
 
-        # give the server time to start
-        time.sleep(2)
+    def assert_index_with_uuids(self, uuid: str) -> None:
+        assert self.index.get_access(uuid) == uuids[uuid]["access"]
+        assert self.index.get_filesize(uuid) == uuids[uuid]["file_size"]
+        assert self.index.get_md5sum(uuid) == uuids[uuid]["md5sum"]
+        assert self.index.get_related_files(uuid) == uuids[uuid]["related_files"]
+        assert self.index.get_annotations(uuid) == uuids[uuid]["annotations"]
 
-    def tearDown(self):
-        self.server.terminate()
-
-    ############ not set ############
-    def test_no_metadata_get_related_files(self):
-        index = GDCIndexClient(uri=base_url)
-
-        results = index.get_related_files("small")
-        assert results == []
-
-    def test_no_metadata_get_annotations(self):
-        index = GDCIndexClient(uri=base_url)
-
-        results = index.get_annotations("small")
-        assert results == []
-
-    def test_no_metadata_get_md5sum(self):
-        index = GDCIndexClient(uri=base_url)
-
-        results = index.get_md5sum("small")
-        assert results == None
-
-    def test_no_metadata_get_filesize(self):
-        index = GDCIndexClient(uri=base_url)
-
-        results = index.get_filesize("small")
-        assert results == None
-
-    def test_no_metadata_get_filesize(self):
-        index = GDCIndexClient(uri=base_url)
-
-        results = index.get_access("small")
-        assert results == None
+    def assert_invalid_index(self):
+        self.assert_index_with_uuids("invalid")
 
     ############ mock metadata ############
-    def test_full_mock_get_metadata(self):
-        index = GDCIndexClient(uri=base_url)
-        index._get_metadata(["small"])
+    @pytest.mark.parametrize(
+        "uuid", ["small", "small_no_friends", "small_ann", "small_rel"]
+    )
+    def test_full_mock_get_metadata(self, uuid: str) -> None:
+        self.index._get_metadata([uuid])
 
-        assert index.get_access("small") == uuids["small"]["access"]
-        assert index.get_filesize("small") == uuids["small"]["file_size"]
-        assert index.get_md5sum("small") == uuids["small"]["md5sum"]
-        assert index.get_related_files("small") == uuids["small"]["related_files"]
-        assert index.get_annotations("small") == uuids["small"]["annotations"]
+        self.assert_index_with_uuids(uuid)
 
-    def test_no_rel_no_ann_mock_get_metadata(self):
-        index = GDCIndexClient(uri=base_url)
-        index._get_metadata(["small_no_friends"])
-
-        assert (
-            index.get_access("small_no_friends") == uuids["small_no_friends"]["access"]
-        )
-        assert (
-            index.get_filesize("small_no_friends")
-            == uuids["small_no_friends"]["file_size"]
-        )
-        assert (
-            index.get_md5sum("small_no_friends") == uuids["small_no_friends"]["md5sum"]
-        )
-        assert index.get_related_files("small_no_friends") == []
-        assert index.get_annotations("small_no_friends") == []
-
-    def test_ann_mock_get_metadata(self):
-        index = GDCIndexClient(uri=base_url)
-        index._get_metadata(["small_ann"])
-
-        assert index.get_access("small_ann") == uuids["small_ann"]["access"]
-        assert index.get_filesize("small_ann") == uuids["small_ann"]["file_size"]
-        assert index.get_md5sum("small_ann") == uuids["small_ann"]["md5sum"]
-        assert index.get_related_files("small_ann") == []
-        assert index.get_annotations("small_ann") == uuids["small_ann"]["annotations"]
-
-    def test_rel_mock_get_metadata(self):
-        index = GDCIndexClient(uri=base_url)
-        index._get_metadata(["small_rel"])
-
-        assert index.get_access("small_rel") == uuids["small_rel"]["access"]
-        assert index.get_filesize("small_rel") == uuids["small_rel"]["file_size"]
-        assert index.get_md5sum("small_rel") == uuids["small_rel"]["md5sum"]
-        assert (
-            index.get_related_files("small_rel") == uuids["small_rel"]["related_files"]
-        )
-        assert index.get_annotations("small_rel") == []
-
-    ############ mock separate small files (smalls) ############
-    def test_small_full_separate_small_files(self):
+    ############ mock separate files ############
+    @pytest.mark.parametrize(
+        "input_uuids,expected_bigs,expected_smalls",
+        [
+            (["small"], ["small"], []),
+            (["small_no_friends"], [], [["small_no_friends"]]),
+            (["big"], ["big"], []),
+            (["big", "small"], ["big", "small"], []),
+            (
+                ["big_no_friends", "small_no_friends"],
+                ["big_no_friends"],
+                [["small_no_friends"]],
+            ),
+        ],
+    )
+    def test_full_separate_files(
+        self,
+        input_uuids: List[str],
+        expected_bigs: List[str],
+        expected_smalls: List[List[str]],
+    ) -> None:
         """ Currently if a file has related or annotation files
         the dtt processes it as if it were a big file so that
         it goes through the old method of downloading,
@@ -120,128 +63,36 @@ class QueryIndexTest(TestCase):
         NOTE: This will probably change in the future.
         """
 
-        index = GDCIndexClient(uri=base_url)
-        bigs, smalls = index.separate_small_files(["small"], HTTP_CHUNK_SIZE)
+        bigs, smalls = self.index.separate_small_files(input_uuids, HTTP_CHUNK_SIZE)
 
-        assert index.get_access("small") == uuids["small"]["access"]
-        assert index.get_filesize("small") == uuids["small"]["file_size"]
-        assert index.get_md5sum("small") == uuids["small"]["md5sum"]
-        assert index.get_related_files("small") == uuids["small"]["related_files"]
-        assert index.get_annotations("small") == uuids["small"]["annotations"]
+        for uuid in input_uuids:
+            self.assert_index_with_uuids(uuid)
 
-        assert bigs == ["small"]
-        assert smalls == []
+        assert set(bigs) == set(expected_bigs)
+        assert smalls == expected_smalls
 
-    def test_small_no_rel_no_ann_separate_small_files(self):
-        index = GDCIndexClient(uri=base_url)
-        bigs, smalls = index.separate_small_files(["small_no_friends"], HTTP_CHUNK_SIZE)
+    ############ not set ############
+    def test_no_metadata(self) -> None:
+        self.assert_invalid_index()
 
-        assert (
-            index.get_access("small_no_friends") == uuids["small_no_friends"]["access"]
-        )
-        assert (
-            index.get_filesize("small_no_friends")
-            == uuids["small_no_friends"]["file_size"]
-        )
-        assert (
-            index.get_md5sum("small_no_friends") == uuids["small_no_friends"]["md5sum"]
-        )
-        assert index.get_related_files("small_no_friends") == []
-        assert index.get_annotations("small_no_friends") == []
-
-        assert bigs == []
-        assert smalls == [["small_no_friends"]]
-
-    def test_small_invalid_separate_small_files(self):
+    def test_small_invalid_separate_small_files(self) -> None:
         """ If no metadata can be found about a file, attempt a
         download using the big file method
         """
 
         invalid = "invalid uuid"
 
-        index = GDCIndexClient(uri=base_url)
-        bigs, smalls = index.separate_small_files([invalid], HTTP_CHUNK_SIZE)
+        bigs, smalls = self.index.separate_small_files([invalid], HTTP_CHUNK_SIZE)
 
-        assert index.get_access(invalid) == None
-        assert index.get_filesize(invalid) == None
-        assert index.get_md5sum(invalid) == None
-        assert index.get_related_files(invalid) == []
-        assert index.get_annotations(invalid) == []
+        self.assert_invalid_index()
 
         assert bigs == [invalid]
         assert smalls == []
 
-    ############ mock separate small files (bigs) ############
-    def test_big_full_separate_small_files(self):
-        index = GDCIndexClient(uri=base_url)
-        bigs, smalls = index.separate_small_files(["big"], HTTP_CHUNK_SIZE)
-
-        assert index.get_access("big") == uuids["big"]["access"]
-        assert index.get_filesize("big") == uuids["big"]["file_size"]
-        assert index.get_md5sum("big") == uuids["big"]["md5sum"]
-        assert index.get_related_files("big") == uuids["big"]["related_files"]
-        assert index.get_annotations("big") == uuids["big"]["annotations"]
-
-        assert bigs == ["big"]
-        assert smalls == []
-
-    ############ mock separate small files (bigs) ############
-    def test_big_and_small_full_separate_small_files(self):
-        index = GDCIndexClient(uri=base_url)
-        bigs, smalls = index.separate_small_files(["big", "small"], HTTP_CHUNK_SIZE)
-
-        assert index.get_access("big") == uuids["big"]["access"]
-        assert index.get_filesize("big") == uuids["big"]["file_size"]
-        assert index.get_md5sum("big") == uuids["big"]["md5sum"]
-        assert index.get_related_files("big") == uuids["big"]["related_files"]
-        assert index.get_annotations("big") == uuids["big"]["annotations"]
-
-        assert index.get_access("small") == uuids["small"]["access"]
-        assert index.get_filesize("small") == uuids["small"]["file_size"]
-        assert index.get_md5sum("small") == uuids["small"]["md5sum"]
-        assert index.get_related_files("small") == uuids["small"]["related_files"]
-        assert index.get_annotations("small") == uuids["small"]["annotations"]
-
-        # if a uuid has related files or annotations then they
-        # are downloaded as big files
-        assert set(bigs) == set(["big", "small"])
-        assert smalls == []
-
-    def test_big_and_small_no_rel_no_ann_separate_small_files(self):
-        index = GDCIndexClient(uri=base_url)
-        bigs, smalls = index.separate_small_files(
-            ["big_no_friends", "small_no_friends"], HTTP_CHUNK_SIZE
-        )
-
-        assert index.get_access("big_no_friends") == uuids["big_no_friends"]["access"]
-        assert (
-            index.get_filesize("big_no_friends") == uuids["big_no_friends"]["file_size"]
-        )
-        assert index.get_md5sum("big_no_friends") == uuids["big_no_friends"]["md5sum"]
-        assert index.get_related_files("big_no_friends") == []
-        assert index.get_annotations("big_no_friends") == []
-
-        assert (
-            index.get_access("small_no_friends") == uuids["small_no_friends"]["access"]
-        )
-        assert (
-            index.get_filesize("small_no_friends")
-            == uuids["small_no_friends"]["file_size"]
-        )
-        assert (
-            index.get_md5sum("small_no_friends") == uuids["small_no_friends"]["md5sum"]
-        )
-        assert index.get_related_files("small_no_friends") == []
-        assert index.get_annotations("small_no_friends") == []
-
-        assert bigs == ["big_no_friends"]
-        assert smalls == [["small_no_friends"]]
-
 
 @pytest.mark.parametrize("case", [range(1), range(499), range(500), range(1000),])
-def test_chunk_list(case):
-    for chunk in _chunk_list(case):
-        assert len(chunk) <= 500
+def test_chunk_list(case: Iterable[int]) -> None:
+    assert all(len(chunk) <= 500 for chunk in _chunk_list(case))
 
 
 @pytest.mark.parametrize(
@@ -252,7 +103,12 @@ def test_chunk_list(case):
         (["1", "2", "3"], ["a", "b", None], {"1": "a", "2": "b", "3": "3"}),
     ],
 )
-def test_get_latest_versions(versions_response, ids, latest_ids, expected):
+def test_get_latest_versions(
+    versions_response,
+    ids: List[str],
+    latest_ids: List[str],
+    expected: Mapping[str, str],
+) -> None:
     url = "https://example.com"
     versions_response(url + "/files/versions", ids, latest_ids)
 
