ea49fe602bc5189ab007ec15ec7ce69fb63a528b
asurin <qiaozha@gmail.com>
2020-05-22 23:28:26 -0500

    DEV-139 add type annotation

--- tests/test_download_client.py
+++ tests/test_download_client.py
@@ -1,5 +1,6 @@
 from multiprocessing import cpu_count
 import os
+from pathlib import Path
 import pytest
 import tarfile
 
@@ -16,14 +17,14 @@ BASE_URL = "http://127.0.0.1:5000"
 @pytest.mark.usefixtures("setup_mock_server")
 class TestDownloadClient:
     @pytest.fixture(autouse=True)
-    def setup_method_fixture(self, tmp_path):
+    def setup_method_fixture(self, tmp_path: Path) -> None:
         self.index_client = GDCIndexClient(BASE_URL)
         self.tmp_path = tmp_path
         # use str version to be 3.5 compatible
         self.client_kwargs = self.get_client_kwargs(str(self.tmp_path))
         self.client = self.get_download_client()
 
-    def get_client_kwargs(self, path):
+    def get_client_kwargs(self, path: str) -> dict:
         return {
             "token": "valid token",
             "n_procs": min(cpu_count(), 8),
@@ -40,32 +41,32 @@ class TestDownloadClient:
             "verify": True,
         }
 
-    def get_download_client(self):
+    def get_download_client(self) -> GDCHTTPDownloadClient:
         return GDCHTTPDownloadClient(
             uri=BASE_URL, index_client=self.index_client, **self.client_kwargs
         )
 
-    def test_download_files_with_fake_uuid_throw_exception_to_developer(self):
+    def test_download_files_with_fake_uuid_throw_exception_to_developer(self) -> None:
         url_with_fake_uuid = BASE_URL + "/data/fake-uuid"
 
         with pytest.raises(RuntimeError):
             self.client.download_files([url_with_fake_uuid])
 
-    def test_download_files_with_fake_uuid_not_throw_exception_to_user(self):
+    def test_download_files_with_fake_uuid_not_throw_exception_to_user(self) -> None:
         url_with_fake_uuid = BASE_URL + "/data/fake-uuid"
 
         self.client_kwargs["debug"] = False
         client_with_debug_off = self.get_download_client()
         client_with_debug_off.download_files([url_with_fake_uuid])
 
-    def test_fix_url(self):
+    def test_fix_url(self) -> None:
         fixed_url = "https://api.gdc.cancer.gov/"
 
         assert fix_url("api.gdc.cancer.gov") == fixed_url
         assert fix_url(fixed_url) == fixed_url
         assert fix_url("api.gdc.cancer.gov/") == fixed_url
 
-    def test_untar_file(self):
+    def test_untar_file(self) -> None:
 
         files_to_tar = ["small", "small_ann", "small_rel", "small_no_friends"]
         tarfile_name = make_tarfile(files_to_tar)
@@ -73,7 +74,7 @@ class TestDownloadClient:
 
         assert all((self.tmp_path / f).exists() for f in files_to_tar)
 
-    def test_md5_members(self):
+    def test_md5_members(self) -> None:
 
         files_to_tar = ["small", "small_ann", "small_rel", "small_no_friends"]
         self.index_client._get_metadata(files_to_tar)
@@ -86,7 +87,7 @@ class TestDownloadClient:
 
         assert errors == []
 
-    def test_download_tarfile(self):
+    def test_download_tarfile(self) -> None:
         # this is done after the small file sorting happens,
         # so pick UUIDs that would be grouped together
         files_to_dl = ["small_no_friends"]
@@ -107,7 +108,7 @@ class TestDownloadClient:
                 contents = t.extractfile(member).read().decode()
                 assert contents == uuids[member.name]["contents"]
 
-    def test_download_annotations(self):
+    def test_download_annotations(self) -> None:
 
         # uuid of file that has an annotation
         small_ann = "small_ann"
@@ -134,7 +135,7 @@ class TestDownloadClient:
         ), "annotations content incorrect"
 
     @pytest.mark.parametrize("check_segments", (True, False))
-    def test_no_segment_md5sums_args(self, check_segments):
+    def test_no_segment_md5sums_args(self, check_segments: bool) -> None:
 
         self.client_kwargs["segment_md5sums"] = check_segments
         self.get_download_client()
