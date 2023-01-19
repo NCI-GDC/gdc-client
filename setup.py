58dee79b92d1de5f7b3d9f1327c606a0c3ea66cb
Qiao <qiaoq@uchicago.edu>
2023-01-19 20:48:41 +0000

    standadize dependencies

--- setup.py
+++ setup.py
@@ -7,4 +7,17 @@ setup(
     packages=find_packages(),
     package_data={},
     scripts=["bin/gdc-client"],
+    install_requires=[
+        "cryptography~=2.8",
+        "jsonschema~=2.6.0",
+        "lxml~=4.4.2",
+        "ndg-httpsclient~=0.5.0",
+        "pyasn1~=0.4.3",
+        "pyOpenSSL~=18.0.0",
+        "PyYAML>=5.1",
+        "intervaltree~=3.0.2",
+        "termcolor~=1.1.0",
+        "requests~=2.22.0",
+        "progressbar2~=3.43.1",
+    ],
 )
