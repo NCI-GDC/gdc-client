9cdac5daf3729342ddb358a5f3d4a86d839928f3
jiakf <jkfeng@uchicago.edu>
2024-03-27 16:40:12 -0500

    put back git versioning and add git setuptools to deps for tox test

--- setup.py
+++ setup.py
@@ -1,49 +1,49 @@
-# import os
-# import re
+import os
+import re
 
 from setuptools import setup, find_packages
 
-# from setuptools_scm import Configuration
-# from setuptools_scm._get_version_impl import parse_version
+from setuptools_scm import Configuration
+from setuptools_scm._get_version_impl import parse_version
 
 
-# def get_version() -> str:
-#     """Create version string based on branch name.
+def get_version() -> str:
+    """Create version string based on branch name.
 
-#     release branch -> 1.2.3rc4
-#     develop branch -> 1.2.3a4
-#     other branch -> 1.2.3.dev4+<branch_name>
+    release branch -> 1.2.3rc4
+    develop branch -> 1.2.3a4
+    other branch -> 1.2.3.dev4+<branch_name>
 
-#     Returns:
-#         version
-#     """
-#     config = Configuration()
-#     version = parse_version(config)
-#     # Handle version.distance None or any other unexpected falsy value that cannot be coerced to int
-#     if not version.distance:
-#         return str(version.tag)
+    Returns:
+        version
+    """
+    config = Configuration()
+    version = parse_version(config)
+    # Handle version.distance None or any other unexpected falsy value that cannot be coerced to int
+    if not version.distance:
+        return str(version.tag)
 
-#     from setuptools_scm.version import guess_next_version
+    from setuptools_scm.version import guess_next_version
 
-#     branch = os.getenv("CI_COMMIT_REF_NAME", version.branch)
-#     if branch.startswith("release"):
-#         fmt = "{guessed}rc{distance}"
-#     elif branch.startswith("develop"):
-#         fmt = "{guessed}a{distance}"
-#     else:
-#         local = re.sub("[^0-9a-zA-Z]+", ".", branch)
-#         fmt = f"{{guessed}}.dev{{distance}}+{local}"
+    branch = os.getenv("CI_COMMIT_REF_NAME", version.branch)
+    if branch.startswith("release"):
+        fmt = "{guessed}rc{distance}"
+    elif branch.startswith("develop"):
+        fmt = "{guessed}a{distance}"
+    else:
+        local = re.sub("[^0-9a-zA-Z]+", ".", branch)
+        fmt = f"{{guessed}}.dev{{distance}}+{local}"
 
-#     return version.format_next_version(guess_next_version, fmt)
+    return version.format_next_version(guess_next_version, fmt)
 
 
 setup(
     name="gdc_client",
-    # use_scm_version={
-    #     "local_scheme": "dirty-tag",
-    # },
-    # setup_requires=["setuptools_scm>8", "setuptools_git_versioning"],
-    # setuptools_git_versioning={"enabled": True, "version_callback": get_version},
+    use_scm_version={
+        "local_scheme": "dirty-tag",
+    },
+    # setup_requires=["setuptools_scm>8"],
+    setuptools_git_versioning={"enabled": True, "version_callback": get_version},
     packages=find_packages(),
     package_data={},
     scripts=["bin/gdc-client"],
