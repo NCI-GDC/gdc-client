1e519be9f091923baffd88303523a621f1e5380d
jiakf <jkfeng@uchicago.edu>
2024-03-27 12:55:41 -0500

    add setuptools_git_versioning configs to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,40 @@
+import os
+import re
+
 from setuptools import setup, find_packages
+from setuptools_scm import Configuration
+from setuptools_scm._get_version_impl import parse_version
+
+
+def get_version() -> str:
+    """Create version string based on branch name.
+
+    release branch -> 1.2.3rc4
+    develop branch -> 1.2.3a4
+    other branch -> 1.2.3.dev4+<branch_name>
+
+    Returns:
+        version
+    """
+    config = Configuration()
+    version = parse_version(config)
+    # Handle version.distance None or any other unexpected falsy value that cannot be coerced to int
+    if not version.distance:
+        return str(version.tag)
+
+    from setuptools_scm.version import guess_next_version
+
+    branch = os.getenv("CI_COMMIT_REF_NAME", version.branch)
+    if branch.startswith("release"):
+        fmt = "{guessed}rc{distance}"
+    elif branch.startswith("develop"):
+        fmt = "{guessed}a{distance}"
+    else:
+        local = re.sub("[^0-9a-zA-Z]+", ".", branch)
+        fmt = f"{{guessed}}.dev{{distance}}+{local}"
+
+    return version.format_next_version(guess_next_version, fmt)
+
 
 setup(
     name="gdc_client",
@@ -6,6 +42,7 @@ setup(
         "local_scheme": "dirty-tag",
     },
     setup_requires=["setuptools_scm<6"],
+    setuptools_git_versioning={"enabled": True, "version_callback": get_version},
     packages=find_packages(),
     package_data={},
     scripts=["bin/gdc-client"],
