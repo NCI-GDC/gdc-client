c9a8dc77794575e4720eca3f37839bcac4ba0c6c
Qiao Qiao <qiaoq@uchicago.edu>
2024-07-23 11:45:54 -0500

    update cryptography

--- setup.py
+++ setup.py
@@ -18,11 +18,11 @@ setup(
     package_data={},
     scripts=["bin/gdc-client"],
     install_requires=[
-        "cryptography~=2.8",
+        "cryptography",
         "jsonschema~=2.6.0",
         "lxml~=4.4.2",
         "ndg-httpsclient~=0.5.0",
-        "pyasn1~=0.4.3",
+        "pyasn1",
         "pyOpenSSL~=18.0.0",
         "PyYAML>=5.1",
         "intervaltree~=3.0.2",
