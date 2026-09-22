454305bf949aa9deaf17a1d8a1783890e69927cc
Alexander Anderson <AlexanderAndersonProgramming@gmail.com>
2026-09-22 08:42:17 -0500

    Feat/dev 4238 context managers (#303)
    * DEV-4238 Add some context managers
    
    * DEV-4238 More context managers
    
    * DEV-4238 Use better variable names
    
    * DEV-4238 Add more type hinting
    
    * DEV-4238 Close context before opening next context
    
    * DEV-4238 Use thread-local storage for sessions
    
    * DEV-4238 Close response in case of error
    
    * DEV-4238 Use context manager everywhere
    
    * DEV-4238 Catch another context that needs managed
    
    * DEV-4238 Add type hinting
    
    * DEV-4238 Apply updates to clarify behavior
    
    * DEV-4238 Adjust line lengths to match google style guide
    
    * DEV-4238 Fix indentions
    
    * DEV-4238 Remove if statement
    
    * DEV-4238 Add error handling
    
    ---------
    
    Co-authored-by: Alexander Anderson <alexander8@uchicago.edu>

--- src/gdc_client/parcel/download_stream.py
+++ src/gdc_client/parcel/download_stream.py
@@ -6,9 +6,13 @@
 # Availability: https://github.com/LabAdvComp/parcel
 # ***************************************************************************************
 
+import contextlib
 import logging
 import os
+import threading
 import time
+import types
+from typing import Any
 from urllib.parse import urlparse
 
 import requests
@@ -16,6 +20,41 @@ from intervaltree import Interval
 
 from gdc_client.parcel import const, utils
 from gdc_client.parcel.defaults import max_timeout
+
+
+class SessionCache:
+    """A thread-safe cache for managing HTTP session objects.
+
+    This class sets up a single thread to keep a session to a single thread to
+    avoid race conditions and uses ExitStack to manage context so connections
+    are closed as expected.
+    """
+
+    def __init__(self) -> None:
+        self._lock = threading.Lock()
+        self._context = contextlib.ExitStack()
+        self._local = threading.local()
+
+    def close(self) -> None:
+        """Closes all sessions registered through the class."""
+        self._context.close()
+
+    def get_session(self) -> requests.Session:
+        """Creates or retrieves a thread-local session.
+
+        If a session does not already exist for the current thread, a new
+        request.Session instance is created and added to the ExitStack context
+        to manage closing out connections as expected, including during error
+        handling.
+
+        Returns:
+            requests.Session: A session object isolated to the local thread.
+        """
+        with self._lock:
+            if not hasattr(self._local, "session"):
+                self._local.session = self._context.enter_context(requests.Session())
+
+            return self._local.session
 
 
 class DownloadStream:
@@ -33,12 +72,24 @@ class DownloadStream:
         self.token = token
         self.url = url
         self.check_file_md5sum = True
+        self._session_cache = SessionCache()
 
     def init(self):
         self.get_information()
         self.print_download_information()
         self.initialized = True
         return self
+
+    def __enter__(self):
+        return self
+
+    def __exit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: types.TracebackType | None,
+    ) -> None:
+        self._session_cache.close()
 
     def _get_directory_name(self, directory, url):
         # get filename/id
@@ -133,7 +184,10 @@ class DownloadStream:
             header["host"] = host
         return header
 
-    def request(self, headers=None, verify=True, close=False, max_retries=16):
+    @contextlib.contextmanager
+    def request(
+        self, headers: dict[str, Any], verify: bool = True, max_retries: int = 16
+    ) -> contextlib.AbstractContextManager[requests.Response]:
         """Make request for file and return the response.
 
         :param str file_id: The id of the entity being requested.
@@ -147,33 +201,30 @@ class DownloadStream:
         """
         self.log.debug(f"Request to {self.url}")
 
-        # Set urllib3 retries and mount for session
-        a = requests.adapters.HTTPAdapter(max_retries=max_retries)
-        s = requests.Session()
-        s.mount(urlparse(self.url).scheme, a)
+        session = self._session_cache.get_session()
 
-        headers = self.headers() if headers is None else headers
+        session.mount(
+            urlparse(self.url).scheme, requests.adapters.HTTPAdapter(max_retries=max_retries)
+        )
+
+        response = None
         try:
-            r = s.get(
+            with session.get(
                 self.url,
                 headers=headers,
                 verify=verify,
                 stream=True,
                 timeout=max_timeout,
-            )
+            ) as response:
+                response.raise_for_status()
+
+                yield response
+
         except Exception as e:
             raise RuntimeError(
                 f"Unable to connect to API: ({e!s}). Is this url correct: '{self.url}'? "
                 "Is there a connection to the API? Is the server running?"
             )
-        try:
-            r.raise_for_status()
-        except Exception as e:
-            raise RuntimeError(f"{e!s}: {r.text}")
-
-        if close:
-            r.close()
-        return r
 
     def get_information(self):
         """Make a request to the data server for information on the file.
@@ -184,10 +235,11 @@ class DownloadStream:
         """
 
         headers = self.header()
-        r = self.request(headers, close=True)
-        self.log.debug("Request responded")
+        with self.request(headers) as response:
+            response.raise_for_status()
+            self.log.debug("Request responded")
 
-        content_length = r.headers.get("Content-Length")
+        content_length = response.headers.get("Content-Length")
         if not content_length:
             self.log.debug("Missing content length.")
             # it also won't come with an md5sum
@@ -196,7 +248,7 @@ class DownloadStream:
             self.size = int(content_length)
             self.log.debug(f"{self.size} bytes")
 
-        attachment = r.headers.get("content-disposition", None)
+        attachment = response.headers.get("content-disposition", None)
         self.log.debug(f"Attachment:         : {attachment}")
 
         # Some of the filenames are set to be equal to an S3 key, which can
@@ -209,7 +261,7 @@ class DownloadStream:
 
         self.md5sum = None
         if self.check_file_md5sum:
-            self.md5sum = r.headers.get("content-md5", "")
+            self.md5sum = response.headers.get("content-md5", "")
 
         return self.name, self.size
 
@@ -235,27 +287,27 @@ class DownloadStream:
 
         try:
             # Initialize segment request
-            r = self.request(self.header(start, end))
+            with self.request(self.header(start, end)) as response:
+                response.raise_for_status()
+                # Iterate over the data stream
+                self.log.debug(f"Initializing segment: {start}-{end}")
+                for chunk in response.iter_content(chunk_size=self.http_chunk_size):
+                    if not chunk:
+                        continue  # Empty are keep-alives.
+                    offset = start + written
 
-            # Iterate over the data stream
-            self.log.debug(f"Initializing segment: {start}-{end}")
-            for chunk in r.iter_content(chunk_size=self.http_chunk_size):
-                if not chunk:
-                    continue  # Empty are keep-alives.
-                offset = start + written
+                    # Write the chunk to disk, create an interval that
+                    # represents the chunk, get md5 info if necessary, and
+                    # report completion back to the producer
+                    utils.write_offset(self.temp_path, chunk, offset)
+                    if self.check_segment_md5sums:
+                        iv_data = {"md5sum": utils.md5sum(chunk)}
+                    else:
+                        iv_data = None
+                    complete_segment = Interval(offset, offset + len(chunk), iv_data)
+                    q_complete.put(complete_segment)
 
-                # Write the chunk to disk, create an interval that
-                # represents the chunk, get md5 info if necessary, and
-                # report completion back to the producer
-                utils.write_offset(self.temp_path, chunk, offset)
-                if self.check_segment_md5sums:
-                    iv_data = {"md5sum": utils.md5sum(chunk)}
-                else:
-                    iv_data = None
-                complete_segment = Interval(offset, offset + len(chunk), iv_data)
-                q_complete.put(complete_segment)
-
-                written += len(chunk)
+                    written += len(chunk)
 
         except KeyboardInterrupt:
             return self.log.error("Process stopped by user.")
@@ -288,7 +340,6 @@ class DownloadStream:
             else:
                 raise RuntimeError("Segment corruption. Max retries exceeded.")
 
-        r.close()
         return written
 
     def print_download_information(self):
